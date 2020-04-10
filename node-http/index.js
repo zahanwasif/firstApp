08a37c4ed3c119012a7ed5a80e1e2e7716c6f0f6
zahanwasif <21100213@lums.edu.pk>
2020-04-10 08:24:06 +0500

    added dependencies

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,44 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-	console.log(req.headers);
+	console.log('Request for ' + req.url + ' by method ' + req.method);
 
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<html><body><h1>Hello World</h1></body></html>');
+	if (req.method == 'GET') {
+		var fileUrl;
+		if (req.url == '/') fileUrl = '/index.html';
+		else fileUrl = req.url;
+
+		var filePath = path.resolve('./public' + fileUrl);
+		var fileExt = path.extname(filePath);
+		if (fileExt == '.html') {
+			fs.exists(filePath, (exists) => {
+				if (!exists) {
+					res.statusCode = 404;
+					res.setHeader('Content-Type', 'text/html');
+					res.end('<html><title>Status 404 : ' + fileUrl + '</title><body><h1>File does not exist</h1></body></html>');
+					return;
+				}
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/html');
+				fs.createReadStream(filePath).pipe(res);
+			});
+		} else {
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/html');
+			res.end('<html><title>Status 404 : ' + fileUrl + '</title><body><h1>File is not html</h1></body></html>');
+			return;
+		}
+	} else {
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'text/html');
+		res.end('<html><title>Status 404 : ' + fileUrl + '</title><body><h1>The method is not GET</h1></body></html>');
+		return;
+	}
 });
 
 server.listen(port, hostname, () => {
